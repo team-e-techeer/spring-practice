1dd97a0207b2484654a400048b9a92e3303eb41a
Joe <at365@naver.com>
2022-04-15 01:27:15 +0900

    feat: Add user entity

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
@@ -1,4 +1,36 @@
 package net.schoolvery.schoolveryserver.domain.user.entity;
 
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Table(name="user_test")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id = UUID.randomUUID();
+
+    private String name;
+    private String nickname;
+    private int phone;
+    private int student_id;
+    private String school;
+    private String email;
+    private String password;
+
+    public User(String name, String nickname, int phone, int student_id, String school, String email, String password) {
+        this.name = name;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.student_id = student_id;
+        this.school = school;
+        this.email = email;
+        this.password = password;
+    }
 }
