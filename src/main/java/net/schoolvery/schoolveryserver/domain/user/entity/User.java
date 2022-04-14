9b73fb78caf22e244b676e6d4cf2ac72f4b5b0c7
SeungWookHan <hswook12@me.com>
2022-04-15 00:48:52 +0900

    Add user model

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
@@ -1,4 +1,52 @@
 package net.schoolvery.schoolveryserver.domain.user.entity;
 
 public class User {
-}
+    private int userPk;
+    private String userName;
+    private String userId;
+    private String userPassword;
+
+    public User() {
+    }
+
+    public User(int userPk, String userName, String userId, String userPassword) {
+        this.userPk = userPk;
+        this.userName = userName;
+        this.userId = userId;
+        this.userPassword = userPassword;
+    }
+
+    public int getUserPk() {
+        return userPk;
+    }
+
+    public void setUserPk(int userPk) {
+        this.userPk = userPk;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+
+};
