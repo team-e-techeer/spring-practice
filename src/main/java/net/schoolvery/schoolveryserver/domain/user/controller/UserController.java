ba50a907cc848a51838ef1265fc1bd4ee1686a63
SeungWookHan <hswook12@me.com>
2022-04-15 00:29:55 +0900

    Add test get api

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
@@ -1,4 +1,15 @@
 package net.schoolvery.schoolveryserver.domain.user.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
+
+    @GetMapping("")
+    public String test(){
+        return "root url call";
+    }
 }
