03bf1bce602a72f9af1da8e044966c78bc1d7b42
Joe <at365@naver.com>
2022-04-15 01:26:10 +0900

    feat: Add user Service

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
@@ -1,4 +1,35 @@
 package net.schoolvery.schoolveryserver.domain.user.service;
 
+import lombok.RequiredArgsConstructor;
+import net.schoolvery.schoolveryserver.domain.user.dto.request.UserCreateDto;
+import net.schoolvery.schoolveryserver.domain.user.dto.response.UserResponseDto;
+import net.schoolvery.schoolveryserver.domain.user.entity.User;
+import net.schoolvery.schoolveryserver.domain.user.exception.UserNotFoundException;
+import net.schoolvery.schoolveryserver.domain.user.repository.UserRopository;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRopository userRopository;
+
+    // User create
+    @Transactional
+    public String CreateUser(UserCreateDto userCreateDto) throws UserNotFoundException {
+        User user = new User(userCreateDto.getName(), userCreateDto.getNickname(),
+                userCreateDto.getPhone(), userCreateDto.getStudent_id(),
+                userCreateDto.getSchool(), userCreateDto.getEmail(),
+                userCreateDto.getPassword());
+
+        User savedUser = userRopository.save(user);
+        return "저장되었습니다.";
+    }
+
+
+
 }
