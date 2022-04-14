5c54377aa25c931c91d156c656d7df56d3542055
Joe <at365@naver.com>
2022-04-15 01:26:55 +0900

    Add user repository

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRopository.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/repository/UserRopository.java
@@ -1,4 +1,12 @@
 package net.schoolvery.schoolveryserver.domain.user.repository;
 
-public interface UserRopository {
+import net.schoolvery.schoolveryserver.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRopository extends JpaRepository<User, UUID> {
+
 }
