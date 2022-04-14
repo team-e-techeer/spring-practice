c0f13d07ee018c072b1d40efd990ec8a1f40bca7
SeungWookHan <hswook12@me.com>
2022-04-15 00:41:00 +0900

    Add test get api by using controller with service

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
@@ -1,15 +1,30 @@
 package net.schoolvery.schoolveryserver.domain.user.controller;
 
+import net.schoolvery.schoolveryserver.domain.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("")
     public String test(){
         return "root url call";
     }
+
+    @GetMapping("/test")
+    public Map<String, String> testMethod() {
+        Map<String, String> res = this.userService.getTest();
+
+        return res;
+    }
+
 }
--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
@@ -1,4 +1,17 @@
 package net.schoolvery.schoolveryserver.domain.user.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class UserService {
+
+    public Map<String, String> getTest() {
+        Map<String, String> res = new HashMap<>();
+        res.put("test", "hi");
+
+        return res;
+    }
 }
