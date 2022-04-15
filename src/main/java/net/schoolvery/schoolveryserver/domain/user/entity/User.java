998fea68872ea6a7e5b316858be6c2d7ad666833
Judy <judyb0626@gmail.com>
2022-04-15 05:15:55 -0700

    chore: Delete unnecessary annotation

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
@@ -19,34 +19,10 @@ public class User {
     private String firstName;
     @NotNull
     private String lastName;
-//    @NotNull
-//    private String email;
-//    @Setter
-//    @NotNull
-//    @Column(length = 128)
-//    private String password;
-//
-//    @NotNull
-//    @Column(length = 128)
-//    private String phoneNumber;
 
-
-//    @Builder
-//    public User(Long id, String firstName, String lastName, String email, String password, String phoneNumber) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//        this.password = password;
-//        this.phoneNumber = phoneNumber;
-//    }
     public void update(String firstName, String lastName) {
-
-//        this.email = email;
-//        this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
-//        this.phoneNumber = phoneNumber;
     }
 
 }
--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserServiceImpl.java
@@ -20,36 +20,27 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    // C : User create
     @Override
     public User create(UserDto dto) {
         User entity = dtoToEntity(dto);
         return userRepository.save(entity);
     }
 
-    // R : User findAll
     @Override
     public List<User> getAllUser() {
         List<User> result = userRepository.findAll();
-//                .stream()
-//                .map(entityToDto())
-//                .collect(Collectors.toList());
         return result;
     }
 
-//     R : User findById
-    // user dto list 주면됨
     @Override
     public UserDto getUser(Long id) {
         Optional<User> result = userRepository.findById(id);
         return result.isPresent()? entityToDto(result.get()): null;
     }
 
-    // U : User update
     @Override
     public UserDto updateUser(Long id, UserDto dto) {
         Optional<User> result = userRepository.findById(id);
-//        if (result.isPresent()) {
         User entity = result.get();
         entity.update(
                 dto.getFirstName(),
@@ -59,7 +50,6 @@ public class UserServiceImpl implements UserService {
         return entityToDto(updatedEntity);
     }
 
-    // D : Delete user by id
     @Override
     public void deleteUser(Long id) {
         userRepository.deleteById(id);
