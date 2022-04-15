241f0f48b0301c209fb19626e2ef98e70fd7f42c
ksy990628 <ksy990628@gmail.com>
2022-04-15 22:27:51 +0900

    chore: Update UserResponseDto

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/controller/UserController.java
@@ -1,4 +1,20 @@
 package net.schoolvery.schoolveryserver.domain.user.controller;
 
+import net.schoolvery.schoolveryserver.domain.user.dto.response.UserResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
+
+    @GetMapping()
+    public ResponseEntity<UserResponseDto> getHello() {
+        UserResponseDto response = UserResponseDto.builder()
+                .result("Hello")
+                .build();
+
+        return ResponseEntity.ok()
+                .body(response);
+    }
 }
