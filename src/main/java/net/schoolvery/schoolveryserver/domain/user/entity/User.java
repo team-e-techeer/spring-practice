6b832be0e52eace67eb40ab1f4a7977eb5bab379
ksy990628 <ksy990628@gmail.com>
2022-04-15 22:27:36 +0900

    chore: Update User

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
@@ -1,4 +1,44 @@
 package net.schoolvery.schoolveryserver.domain.user.entity;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String school;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer email_validation;
+
+    @Column(nullable = false)
+    private Integer password;
+
+    @Column(nullable = false)
+    private Integer provider;
+
+    @Column(nullable = false)
+    private Integer profile_image_url;
+
+    @Column(nullable = false)
+    private Integer phone_num;
 }
