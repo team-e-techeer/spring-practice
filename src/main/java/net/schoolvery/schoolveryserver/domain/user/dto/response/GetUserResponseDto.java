97339c069693253b3823c79418dd12cc78d7d1b0
Joe <at365@naver.com>
2022-04-16 22:43:19 +0900

    feat: Add UserResponseDto

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/dto/response/GetUserResponseDto.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/dto/response/GetUserResponseDto.java
@@ -1,17 +1,21 @@
 package net.schoolvery.schoolveryserver.domain.user.dto.response;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserLoginResponseDto {
+@AllArgsConstructor
+public class GetUserResponseDto {
 
+    private int id ;
+
+    private String name;
+    private String nickname;
+    private int phone;
+    private int student_id;
+    private String school;
     private String email;
     private String password;
-
-
 }
