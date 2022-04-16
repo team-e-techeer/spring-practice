1dab470d9856fda168bf15b8ad99b1f4c04abbf8
Joe <at365@naver.com>
2022-04-16 22:44:57 +0900

    feat: Update userEntity

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/entity/User.java
@@ -2,19 +2,16 @@ package net.schoolvery.schoolveryserver.domain.user.entity;
 
 import lombok.*;
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.util.UUID;
 
 
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Entity
-@Table(name="user_test")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id = UUID.randomUUID();
+    private int id ;
 
     private String name;
     private String nickname;
@@ -24,13 +21,5 @@ public class User {
     private String email;
     private String password;
 
-    public User(String name, String nickname, int phone, int student_id, String school, String email, String password) {
-        this.name = name;
-        this.nickname = nickname;
-        this.phone = phone;
-        this.student_id = student_id;
-        this.school = school;
-        this.email = email;
-        this.password = password;
-    }
+
 }
