658bc4292a815553bcadb5a60f58657d2f84a597
Joe <at365@naver.com>
2022-04-16 22:45:16 +0900

    feat:Update user Service

--- src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
+++ src/main/java/net/schoolvery/schoolveryserver/domain/user/service/UserService.java
@@ -2,34 +2,48 @@ package net.schoolvery.schoolveryserver.domain.user.service;
 
 import lombok.RequiredArgsConstructor;
 import net.schoolvery.schoolveryserver.domain.user.dto.request.UserCreateDto;
+import net.schoolvery.schoolveryserver.domain.user.dto.request.UserUpdateDto;
+import net.schoolvery.schoolveryserver.domain.user.dto.response.GetUserResponseDto;
 import net.schoolvery.schoolveryserver.domain.user.dto.response.UserResponseDto;
 import net.schoolvery.schoolveryserver.domain.user.entity.User;
 import net.schoolvery.schoolveryserver.domain.user.exception.UserNotFoundException;
-import net.schoolvery.schoolveryserver.domain.user.repository.UserRopository;
+import net.schoolvery.schoolveryserver.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.UUID;
+import java.util.List;
 
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRopository userRopository;
+    private final UserRepository userRepository;
 
     // User create
-    @Transactional
-    public String CreateUser(UserCreateDto userCreateDto) throws UserNotFoundException {
-        User user = new User(userCreateDto.getName(), userCreateDto.getNickname(),
-                userCreateDto.getPhone(), userCreateDto.getStudent_id(),
-                userCreateDto.getSchool(), userCreateDto.getEmail(),
-                userCreateDto.getPassword());
+    public UserResponseDto createUser(UserCreateDto userCreateDto) throws UserNotFoundException{
 
-        User savedUser = userRopository.save(user);
-        return "저장되었습니다.";
+        int id = userRepository.createUser(userCreateDto);
+        return new UserResponseDto(id);
+
     }
 
+    // User update
+    public int modifyUser(UserUpdateDto userUpdateDto) {
+        int result = userRepository.modifyUserName(userUpdateDto);
+
+        return result;
+    }
+
+
+    // User read
+    public List<GetUserResponseDto> getUser() {
+        List<GetUserResponseDto> getUserResponseDtoList = userRepository.getUser();
+
+        return getUserResponseDtoList;
+    }
+
+    // User delete
 
 
 }
