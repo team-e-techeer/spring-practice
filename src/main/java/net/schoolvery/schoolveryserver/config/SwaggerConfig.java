482a24a258e91875999721f02a6ce503fa895d8e
Joe <at365@naver.com>
2022-04-18 00:56:44 +0900

    feat:Add Swagger Config

---
+++ src/main/java/net/schoolvery/schoolveryserver/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package net.schoolvery.schoolveryserver.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig  {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("User API template")
+                        .description("User API template Swagger")
+                        .version("v0.0.1"));
+    }
+}
